6bfda29f4e46afe64dc70c41998c5668cbf637bf
Victor Gama <victor@insitelabs.com.br>
2013-09-05 15:12:20 -0300

    Added source

---
+++ identicon.gemspec
@@ -0,0 +1,20 @@
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+Gem::Specification.new do |s|
+	s.name					= "identicon"
+	s.version				= "0.0.1"
+	s.authors				= ["Victor Gama"]
+	s.email					= ["victor@insitelabs.com.br"]
+
+	s.description		= "A simple github-like identicons generator."
+	s.summary			= "A simple github-like identicons generator."
+	s.homepage			= "http://github.com/victorgama/identicons"
+	s.license			= "MIT"
+
+	s.files				= `git ls-files`.split($/)
+	s.executables		= s.files.grep(%r{^bin/}) { |file| File.basename(file) }
+	s.require_paths		= ["lib"]
+
+	s.add_dependency	"chunky_png", "~> 1.2.8"
+end
---
+++ lib/identicon.rb
@@ -0,0 +1,119 @@
+require 'digest'
+require 'chunky_png'
+
+class Identicon
+	class << self 
+		def data_url_for(data, size = 420, background = nil)
+			img = self.new data, size
+			img.background = background unless background.nil?
+			img.generate_image
+			img.to_data_url
+		end
+		def file_for(data, path, size = 420, background = nil)
+			img = self.new data, size
+			img.background = background unless background.nil?
+			img.generate_image
+			img.save(path)
+		end
+	end
+	attr_accessor :size, :data, :background
+	CHARS_REGEX = /(\w)(\w)/
+
+	COLORS = {
+		:a => [196, 108, 168],
+		:b => [127, 219, 107],
+		:c => [83, 89, 215],
+		:d => [111, 198, 148],
+		:e => [186, 130, 82],
+		:f => [113, 66, 197],
+		:"0" => [201, 186, 84],
+		:"1" => [114, 153, 205],
+		:"2" => [141, 108, 204],
+		:"3" => [75, 183, 114],
+		:"4" => [192, 86, 82],
+		:"5" => [199, 115, 209],
+		:"6" => [206, 77, 94],
+		:"7" => [211, 135, 162],
+		:"8" => [71, 194, 197],
+		:"9" => [188, 110, 93]
+	}
+
+	def initialize(data, size)
+		@hash = Digest::MD5.hexdigest(data.to_s)
+		@chars = @hash.scan(CHARS_REGEX)
+		@color = [0, 0, 0]
+		@size = size
+		@pixel_ratio = (size / 6).round
+		@image_size = @size - (@pixel_ratio / 1.5).round
+		@square_array = Hash.new { |h| h = Hash.new(false) }
+		@background = [240, 240, 240]
+		convert_hash
+	end
+
+
+	def size=(value)
+		@size = value
+		@pixel_ratio = (value / 5).round
+		value
+	end
+
+	def data=(value)
+		@hash = Digest::MD5.hexdigest(data.to_s)
+		@chars = @hash.scan(CHARS_REGEX)
+		convert_hash
+		value
+	end
+
+	private
+	def convert_hash
+		chars = @chars.map { |p|
+			p[1]
+		}
+		chars.each_index { |i|
+			char = chars[i]
+			sqIndex = (i / 3).round
+			@square_array[i/3] = [] unless @square_array.member? sqIndex
+
+			if(i % 3 == 0) 
+				@square_array[i/3][0] = h_to_b char
+				@square_array[i/3][4] = h_to_b char
+			elsif(i % 3 == 1) 
+				@square_array[i/3][1] = h_to_b char
+				@square_array[i/3][3] = h_to_b char
+			else
+				@square_array[i/3][2] = h_to_b char
+			end
+		}
+		color = :a
+		0.upto(5) { color = chars.shift.to_sym }
+		@color = COLORS[color]
+	end
+
+	def h_to_b(value)
+		value.bytes.first % 2 == 0
+	end
+
+	public
+	def generate_image
+		pImage = ChunkyPNG::Image.new(@image_size, @image_size, ChunkyPNG::Color::rgb(@background[0], @background[1], @background[2]))
+		@image = ChunkyPNG::Image.new(@size, @size, ChunkyPNG::Color::rgb(@background[0], @background[1], @background[2]))
+		@square_array.each_key { |lineKey|
+			lineValue = @square_array[lineKey]
+			lineValue.each_index { |colKey|
+				colValue = lineValue[colKey]
+				if(colValue)
+					pImage.rect(colKey * @pixel_ratio, lineKey * @pixel_ratio, (colKey + 1) * @pixel_ratio, (lineKey + 1) * @pixel_ratio, ChunkyPNG::Color::TRANSPARENT, ChunkyPNG::Color.rgb(@color[0], @color[1], @color[2]))
+				end
+			}
+		}
+		@image.compose!(pImage, @pixel_ratio / 2, @pixel_ratio / 2)
+	end
+
+	def to_data_url
+		@image.to_data_url
+	end
+
+	def save(path)
+		@image.save(path)
+	end
+end
