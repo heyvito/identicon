d07bdb054bbcace99a4ad2a8c432a13a9c423d75
Victor Gama <hey@vito.io>
2014-01-12 14:34:27 -0200

    Code cleanup

--- identicon.gemspec
+++ identicon.gemspec
@@ -5,16 +5,16 @@ Gem::Specification.new do |s|
 	s.name					= "identicon"
 	s.version				= "0.0.2"
 	s.authors				= ["Victor Gama"]
-	s.email					= ["victor@insitelabs.com.br"]
+	s.email					= ["hey@vito.io"]
 
 	s.description		= "A simple github-like identicons generator."
-	s.summary			= "A simple github-like identicons generator."
+	s.summary			  = "A simple github-like identicons generator."
 	s.homepage			= "http://github.com/victorgama/identicon"
-	s.license			= "MIT"
+	s.license			  = "MIT"
 
-	s.files				= `git ls-files`.split($/)
+	s.files				  = `git ls-files`.split($/)
 	s.executables		= s.files.grep(%r{^bin/}) { |file| File.basename(file) }
-	s.require_paths		= ["lib"]
+	s.require_paths	= ["lib"]
 
 	s.add_dependency	"chunky_png", "~> 1.2.8"
 end
--- lib/identicon.rb
+++ lib/identicon.rb
@@ -3,47 +3,18 @@ require 'chunky_png'
 
 # Generates GitHub-like identicons using MD5 hashes
 class Identicon
-	class << self 
-		# Generates a identicon using the data:image protocol.
-		# Usefull for web applications.
-		#
-		# === Parameters
-		# * data 	= The data that will be converted to a identicon
-		# * size    = (Optional) The image size. Defaults to 420px
-		# * background = (Optional) The background color to be used in a rgb array. ([255, 255, 255])
-
-		def data_url_for(data, size = 420, background = nil)
-			img = self.new data, size
-			img.background = background unless background.nil?
-			img.generate_image
-			img.to_data_url
-		end
-
-		# Generates a identicon and stores it in a file
-		# Usefull for web applications.
-		#
-		# === Parameters
-		# * data 	= The data that will be converted to a identicon
-		# * path    = The path where the file will be stored
-		# * size    = (Optional) The image size. Defaults to 420px
-		# * background = (Optional) The background color to be used in a rgb array. ([255, 255, 255])
-		def file_for(data, path, size = 420, background = nil)
-			img = self.new data, size
-			img.background = background unless background.nil?
-			img.generate_image
-			img.save(path)
-		end
-	end
+  
 	attr_accessor :size, :data, :background
+  
 	CHARS_REGEX = /(\w)(\w)/
 
 	COLORS = {
-		:a => [196, 108, 168],
-		:b => [127, 219, 107],
-		:c => [83, 89, 215],
-		:d => [111, 198, 148],
-		:e => [186, 130, 82],
-		:f => [113, 66, 197],
+		:a =>   [196, 108, 168],
+		:b =>   [127, 219, 107],
+		:c =>   [83, 89, 215],
+		:d =>   [111, 198, 148],
+		:e =>   [186, 130, 82],
+		:f =>   [113, 66, 197],
 		:"0" => [201, 186, 84],
 		:"1" => [114, 153, 205],
 		:"2" => [141, 108, 204],
@@ -55,12 +26,49 @@ class Identicon
 		:"8" => [71, 194, 197],
 		:"9" => [188, 110, 93]
 	}
+  
+	class << self 
+		# Generates a identicon using the data:image protocol.
+		# Usefull for web applications.
+		#
+		# === Parameters
+		#   data 	      - The data that will be converted to a identicon
+		#   size        - (Optional) The image size. Defaults to 420px
+		#   background  - (Optional) The background color to be used in a rgb array. 
+    #                 Ex: [255, 255, 255]
+		def data_url_for(data, size = 420, background = nil)
+			img = self.new(data, size)
+			img.background = background unless background.nil?
+			img.generate_image
+			img.to_data_url
+		end
 
-	# The +new+ method initializes a new Identicon instance
+
+		# Generates a identicon and stores it in a file
+		# Useful for web applications.
+		#
+		# === Parameters
+		#   data 	      - The data that will be converted to a identicon
+		#   path        - The path where the file will be stored
+		#   size        - (Optional) The image size. Defaults to 420px
+		#   background  - (Optional) The background color to be used in a rgb array. 
+    #                 Ex: [255, 255, 255]
+    #
+    # Returns nothing
+		def file_for(data, path, size = 420, background = nil)
+			img = self.new(data, size)
+			img.background = background unless background.nil?
+			img.generate_image
+			img.save(path)
+		end
+	end
+  
+  
+	# Constructs a new Identicon instance
 	#
 	# === Parameters
-	# * data 	= The data that will be converted to a identicon
-	# * size    = The image size
+	# data 	  - The data that will be converted to a identicon
+	# size    - The image size
 	def initialize(data, size)
 		@hash = Digest::MD5.hexdigest(data.to_s)
 		@chars = @hash.scan(CHARS_REGEX)
@@ -80,12 +88,52 @@ class Identicon
 		value
 	end
 
+
 	def data=(value)
 		@hash = Digest::MD5.hexdigest(data.to_s)
 		@chars = @hash.scan(CHARS_REGEX)
 		convert_hash
 		value
 	end
+
+
+	# Process the specified data and generates the result
+	def generate_image
+		pImage = ChunkyPNG::Image.new(@image_size, @image_size, ChunkyPNG::Color::rgb(@background[0], @background[1], @background[2]))
+		@image = ChunkyPNG::Image.new(@size, @size, ChunkyPNG::Color::rgb(@background[0], @background[1], @background[2]))
+		@square_array.each_key { |lineKey|
+			lineValue = @square_array[lineKey]
+			lineValue.each_index { |colKey|
+				colValue = lineValue[colKey]
+				if(colValue)
+					pImage.rect(
+            colKey * @pixel_ratio, 
+            lineKey * @pixel_ratio, 
+            (colKey + 1) * @pixel_ratio, 
+            (lineKey + 1) * @pixel_ratio, 
+            ChunkyPNG::Color::TRANSPARENT, 
+            ChunkyPNG::Color.rgb(@color[0], @color[1], @color[2])
+          )
+				end
+			}
+		}
+		@image.compose!(pImage, @pixel_ratio / 2, @pixel_ratio / 2)
+	end
+
+
+	# Exports the result to a data:image format. 
+	def to_data_url
+		@image.to_data_url
+	end
+
+
+	# Exports the result to a file
+	# === Parameters
+	# path - Path where the image will be stored
+	def save(path)
+		@image.save(path)
+	end
+
 
 	private
 	def convert_hash
@@ -114,34 +162,5 @@ class Identicon
 
 	def h_to_b(value)
 		value.bytes.first % 2 == 0
-	end
-
-	public
-	# Process the specified data and generates the result
-	def generate_image
-		pImage = ChunkyPNG::Image.new(@image_size, @image_size, ChunkyPNG::Color::rgb(@background[0], @background[1], @background[2]))
-		@image = ChunkyPNG::Image.new(@size, @size, ChunkyPNG::Color::rgb(@background[0], @background[1], @background[2]))
-		@square_array.each_key { |lineKey|
-			lineValue = @square_array[lineKey]
-			lineValue.each_index { |colKey|
-				colValue = lineValue[colKey]
-				if(colValue)
-					pImage.rect(colKey * @pixel_ratio, lineKey * @pixel_ratio, (colKey + 1) * @pixel_ratio, (lineKey + 1) * @pixel_ratio, ChunkyPNG::Color::TRANSPARENT, ChunkyPNG::Color.rgb(@color[0], @color[1], @color[2]))
-				end
-			}
-		}
-		@image.compose!(pImage, @pixel_ratio / 2, @pixel_ratio / 2)
-	end
-
-	# Exports the result to a data:image format. 
-	def to_data_url
-		@image.to_data_url
-	end
-
-	# Exports the result to a file
-	# === Parameters
-	# * path	= Path where the image will be stored
-	def save(path)
-		@image.save(path)
 	end
 end
